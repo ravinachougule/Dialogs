f0fdd35d5bf1fd61b3cb0d5e6c4109475edfd3bb
ravinachougule <ravinachougule7@gmail.com>
2018-05-19 20:26:58 +0530

    Dialogs kotlin

--- app/src/main/java/com/example/lenovo/dialogsdemo/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/dialogsdemo/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.lenovo.dialogsdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnAlert.setOnClickListener {
+            showDialog("alert")
+        }
+
+        btnDate.setOnClickListener {
+            showDialog("date")
+        }
+
+        btnTime.setOnClickListener {
+            showDialog("time")
+        }
+
+        btnProg.setOnClickListener {
+            showDialog("progress")
+        }
+
+        btnCust.setOnClickListener {
+
+        }
     }
+
+    private fun showDialog(tag : String) = BlankFragment().show(
+            supportFragmentManager,
+            tag
+    )
 } 
